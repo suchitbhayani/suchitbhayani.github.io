f5f22345dd61cc3cbb72e3d844fae0aff7829d46
Suchit Bhayani <suchit.bhayani@gmail.com>
2025-04-15 16:48:34 -0700

    lab 3, basic JS added

---
+++ global.js
@@ -0,0 +1,59 @@
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume'},
+    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/suchitbhayani/', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"                  // Local server
+    : "/website/";         // GitHub Pages repo name
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+    nav.append(a);
+}  
+
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+        <label class="color-scheme">
+            Theme:
+            <select>
+                <option value="light dark">Automatic</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+            </select>
+        </label>`
+);
+
+let select = document.querySelector('select');
+
+if ('colorScheme' in localStorage) {
+    let scheme = localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', scheme);
+    select.value = scheme;
+}
+
+select.addEventListener('input', function (event) {
+    let scheme = event.target.value;
+    console.log('color scheme changed to', scheme);
+    document.documentElement.style.setProperty('color-scheme', scheme);
+    localStorage.colorScheme = scheme;
+});
