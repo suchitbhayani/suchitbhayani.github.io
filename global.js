e18a14441a8c1c42534bd052366ac5c5ca2e9862
Suchit Bhayani <suchit.bhayani@gmail.com>
2025-04-15 23:51:42 -0700

    formatting fix

--- global.js
+++ global.js
@@ -5,8 +5,8 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
+    { url: '', title: 'Home'},
+    { url: 'projects/', title: 'Projects'},
     { url: 'resume/', title: 'Resume'},
     { url: 'contact/', title: 'Contact'},
     { url: 'https://github.com/suchitbhayani/', title: 'GitHub' }
